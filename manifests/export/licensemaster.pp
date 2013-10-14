dfe242e898f479c3104b18bb1eb4d20577adf3c3
WianVos <wvos@xebia.com>
2013-10-14 13:09:31 +0200

    removed ip adress from license master

--- manifests/export/licensemaster.pp
+++ manifests/export/licensemaster.pp
@@ -1,12 +1,10 @@
 # this class exports resources about searchhead for others to be imported
 class splunk::export::licensemaster (
-  $splk_network_interface = $splunk::splk_network_interface,
   $splk_adminport = $splunk::splk_adminport) {
 
   #   test clause for puppet_config type/resource
   @@splunk::resources::splunk_license_master {$::hostname:
-    network_interface => $splk_network_interface,
-    splunk_admin_port => $splk_adminport
+    port => $splk_adminport
   }
 
 
